90964c66a67e91c910a40ac33f7afb5f16e64e3d
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2019-03-26 08:42:33 -0400

    format

--- src/DisasmData.cpp
+++ src/DisasmData.cpp
@@ -109,8 +109,8 @@ std::string DisasmData::getForwardedSymbolEnding(const gtirb::Symbol* symbol,
     if ((section = this->getSection(".plt")) && containsAddr(*section, addr) &&
         !isAbsolute)
       return std::string{"@PLT"};
-    if ((section = this->getSection(".plt.got")) && containsAddr(*section, addr) &&
-        !isAbsolute)
+    if ((section = this->getSection(".plt.got")) &&
+        containsAddr(*section, addr) && !isAbsolute)
       return std::string{"@PLT"};
     if ((section = this->getSection(".got")) && containsAddr(*section, addr))
       return std::string{"@GOTPCREL"};
