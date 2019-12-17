c5e797bcffc230bead3a65bb4201bd4738b7c336
Antonio Flores Montoya <afloresmontoya@grammatech.com>
2019-12-17 08:43:42 -0500

    explicit path conversion

--- tests/binary_printer_end2end_test.py
+++ tests/binary_printer_end2end_test.py
@@ -3,12 +3,12 @@ from pathlib import Path
 import subprocess
 import sys
 
-two_modules_gtirb=Path('tests/two_modules.gtirb')
+two_modules_gtirb=Path('tests','two_modules.gtirb')
 
 
 class TestBinaryGeneration(unittest.TestCase):
     def test_generate_binary(self): 
-        output= subprocess.check_output(['gtirb-binary-printer','--ir',two_modules_gtirb,'-b','/tmp/two_modules']).decode(sys.stdout.encoding)
+        output= subprocess.check_output(['gtirb-binary-printer','--ir',str(two_modules_gtirb),'-b','/tmp/two_modules']).decode(sys.stdout.encoding)
         self.assertTrue('Calling compiler' in output)
         output_bin = subprocess.check_output('/tmp/two_modules').decode(sys.stdout.encoding)
         self.assertTrue('!!!Hello World!!!' in output_bin)
--- tests/pprinter_end2end_test.py
+++ tests/pprinter_end2end_test.py
@@ -3,23 +3,23 @@ from pathlib import Path
 import subprocess
 import sys
 
-two_modules_gtirb=Path('tests/two_modules.gtirb')
+two_modules_gtirb=Path('tests','two_modules.gtirb')
 
 class TestPrintToStdout(unittest.TestCase):
     def test_print_module0(self): 
-        output= subprocess.check_output(['gtirb-pprinter','--ir',two_modules_gtirb,'-m','0']).decode(sys.stdout.encoding)
+        output= subprocess.check_output(['gtirb-pprinter','--ir',str(two_modules_gtirb),'-m','0']).decode(sys.stdout.encoding)
         self.assertTrue('.globl main' in output)
         self.assertFalse('.globl fun' in output)
     
     def test_print_module1(self): 
-        output= subprocess.check_output(['gtirb-pprinter','--ir',two_modules_gtirb,'-m','1']).decode(sys.stdout.encoding)
+        output= subprocess.check_output(['gtirb-pprinter','--ir',str(two_modules_gtirb),'-m','1']).decode(sys.stdout.encoding)
         self.assertTrue('.globl fun' in output)
         self.assertFalse('.globl main' in output)
     
 
 class TestPrintToFile(unittest.TestCase):
       def test_print_two_modules(self): 
-        output= subprocess.check_output(['gtirb-pprinter','--ir',two_modules_gtirb,'--asm','/tmp/two_modules.s']).decode(sys.stdout.encoding)
+        output= subprocess.check_output(['gtirb-pprinter','--ir',str(two_modules_gtirb),'--asm','/tmp/two_modules.s']).decode(sys.stdout.encoding)
         with open('/tmp/two_modules.s','r') as f:
             self.assertTrue('.globl main' in f.read())
         with open('/tmp/two_modules1.s','r') as f:
