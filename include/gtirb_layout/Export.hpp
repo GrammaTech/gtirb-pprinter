57700cc801336bc4f0c37371698d89e47efb24f5
Joshua Robbins <jrobbins@grammatech.com>
2020-02-14 12:17:55 -0500

    Use better criteria for determining export macro

--- include/gtirb_layout/Export.hpp
+++ include/gtirb_layout/Export.hpp
@@ -30,14 +30,26 @@
 /// DLL's and Linux Shared Objects.
 ///
 
-#ifdef WIN32
+/// @cond INTERNAL
+#ifndef __has_declspec_attribute
+#define __has_declspec_attribute(x) 0
+#endif
+
+#ifndef __has_attribute
+#define __has_attribute(x) 0
+#endif
+/// @endcond
+
+#if defined(_MSC_VER) || __has_declspec_attribute(dllexport)
 #if defined gtirb_layout_EXPORTS
 #define GTIRB_LAYOUT_EXPORT_API _declspec(dllexport)
 #else
 #define GTIRB_LAYOUT_EXPORT_API _declspec(dllimport)
 #endif
-#else
+#elif defined(__GNUC__) || __has_attribute(visibility)
 #define GTIRB_LAYOUT_EXPORT_API __attribute__((visibility("default")))
+#else
+#define GTIRB_LAYOUT_EXPORT_API
 #endif
 
 #endif /* GTIRB_LAYOUT_EXPORT_H */
--- include/gtirb_pprinter/Export.hpp
+++ include/gtirb_pprinter/Export.hpp
@@ -30,14 +30,26 @@
 /// DLL's and Linux Shared Objects.
 ///
 
-#ifdef WIN32
+/// @cond INTERNAL
+#ifndef __has_declspec_attribute
+#define __has_declspec_attribute(x) 0
+#endif
+
+#ifndef __has_attribute
+#define __has_attribute(x) 0
+#endif
+/// @endcond
+
+#if defined(_MSC_VER) || __has_declspec_attribute(dllexport)
 #if defined DEBLOAT_debloatPrettyPrinter_EXPORTS
 #define DEBLOAT_PRETTYPRINTER_EXPORT_API _declspec(dllexport)
 #else
 #define DEBLOAT_PRETTYPRINTER_EXPORT_API _declspec(dllimport)
 #endif
-#else
+#elif defined(__GNUC__) || __has_attribute(visibility)
 #define DEBLOAT_PRETTYPRINTER_EXPORT_API __attribute__((visibility("default")))
+#else
+#define DEBLOAT_PRETTYPRINTER_EXPORT_API
 #endif
 
 #endif /* GTIRB_PP_EXPORT_H */
