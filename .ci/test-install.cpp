0c65ad5fd7aecfedbb2f44d4ed1644398f9fa11e
Brian Fairservice <bfairservice@grammatech.com>
2020-02-17 14:55:53 -0500

    Add job to test installers

---
+++ .ci/test-install.cpp
@@ -0,0 +1,10 @@
+#include <gtirb_pprinter/PrettyPrinter.hpp>
+#include <iostream>
+
+int main()
+{
+  gtirb_pprint::PrettyPrinter pprinter;
+  pprinter.setDebug(true);
+  std::cout<< pprinter.getDebug() << "\n";
+  return 0;
+}
