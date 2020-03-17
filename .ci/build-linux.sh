95b2b17b9760d788e6c512053f5010f369bf5428
Brian Fairservice <bfairservice@grammatech.com>
2020-03-17 11:16:33 -0400

    Use gtirb artifacts during build

--- .ci/build-linux.sh
+++ .ci/build-linux.sh
@@ -9,10 +9,6 @@ set -o pipefail
 
 CXX_COMPILER=$1
 
-# Install GTIRB
-rm -rf /gt/gtirb-pprinter/gtirb/build /gt/gtirb-pprinter/gtirb/CMakeCache.txt /gt/gtirb-pprinter/gtirb/CMakeFiles /gt/gtirb-pprinter/gtirb/CMakeScripts
-cd /gt/gtirb-pprinter/gtirb/ && cmake ./ -Bbuild -DCMAKE_CXX_COMPILER=$CXX_COMPILER && cd build &&  make && make install
-
 # Build gtirb-pprinter
 rm -rf /gt/gtirb-pprinter/build /gt/gtirb-pprinter/CMakeCache.txt /gt/gtirb-pprinter/CMakeFiles /gt/gtirb-pprinter/CMakeScripts
 mkdir -p /gt/gtirb-pprinter/build
