74b95bdd28e2c91fbfc17b1f1f191e32bbe4e71b
Joshua Robbins <jrobbins@grammatech.com>
2020-04-07 08:43:29 -0400

    Fix bug where duplicate bytes would be printed

--- src/gtirb_pprinter/PrettyPrinter.cpp
+++ src/gtirb_pprinter/PrettyPrinter.cpp
@@ -316,6 +316,10 @@ void PrettyPrinterBase::printOverlapWarning(std::ostream& os,
 void PrettyPrinterBase::printBlockContents(std::ostream& os,
                                            const gtirb::CodeBlock& x,
                                            uint64_t offset) {
+  if (offset > x.getSize()) {
+    return;
+  }
+
   gtirb::Addr addr = *x.getAddress();
   printFunctionHeader(os, addr);
   os << '\n';
@@ -622,6 +626,10 @@ void PrettyPrinterBase::printBlock(std::ostream& os,
 void PrettyPrinterBase::printBlockContents(std::ostream& os,
                                            const gtirb::DataBlock& dataObject,
                                            uint64_t offset) {
+  if (offset > dataObject.getSize()) {
+    return;
+  }
+
   printComments(os, gtirb::Offset(dataObject.getUUID(), offset),
                 dataObject.getSize() - offset);
   if (this->debug)
