b7e16e3db6f95e8884f2db10865603ffcd9cba61
Joshua Robbins <jrobbins@grammatech.com>
2020-05-28 13:58:46 -0400

    Reset program counter when starting new section

--- src/gtirb_pprinter/PrettyPrinter.cpp
+++ src/gtirb_pprinter/PrettyPrinter.cpp
@@ -1005,6 +1005,7 @@ void PrettyPrinterBase::printSection(std::ostream& os,
   if (shouldSkip(section)) {
     return;
   }
+  programCounter = gtirb::Addr{0};
 
   printSectionHeader(os, section);
 
