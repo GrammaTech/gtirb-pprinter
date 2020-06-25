63597d24b00e18c0a062df667fd751a894090e61
Jonathan Dorn <jdorn@grammatech.com>
2020-06-25 19:06:55 +0000

    Register layout aux_data types in driver programs

--- src/gtirb_pprinter/driver/pretty_printer.cpp
+++ src/gtirb_pprinter/driver/pretty_printer.cpp
@@ -27,6 +27,7 @@ static fs::path getAsmFileName(const fs::path& InitialPath, int Index) {
 }
 
 int main(int argc, char** argv) {
+  gtirb_layout::registerAuxDataTypes();
   gtirb_pprint::registerAuxDataTypes();
 
   po::options_description desc("Allowed options");
