dc0e6438f9fd78c9ba783b217b5d23bc1c4caa85
Kevin Warrick <kwarrick@grammatech.com>
2020-06-30 16:15:11 -0400

    Update libcapstone version

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -6,7 +6,7 @@ if("${CPACK_GTIRB_DEBIAN_PACKAGE}" STREQUAL "lib")
   set(CPACK_PACKAGE_FILE_NAME "libgtirb-pprinter")
   set(CPACK_COMPONENTS_ALL pprinter_library layout_library)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libstdc++6, libc6, libgcc1, libgtirb (>=${CPACK_GTIRB_VERSION}), libgtirb (<<${CPACK_GTIRB_VERSION_UPPER_BOUND}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt1)"
+      "libstdc++6, libc6, libgcc1, libgtirb (>=${CPACK_GTIRB_VERSION}), libgtirb (<<${CPACK_GTIRB_VERSION_UPPER_BOUND}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt2)"
   )
 elseif("${CPACK_GTIRB_DEBIAN_PACKAGE}" STREQUAL "lib-dbg")
   set(CPACK_DEBIAN_PACKAGE_NAME "libgtirb-pprinter-dbg")
@@ -20,7 +20,7 @@ elseif("${CPACK_GTIRB_DEBIAN_PACKAGE}" STREQUAL "dev")
   set(CPACK_COMPONENTS_ALL pprinter_headers layout_headers cmake_config
                            cmake_target)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libstdc++6, libc6, libgcc1, libgtirb-dev (>=${CPACK_GTIRB_VERSION}), libgtirb-dev (<<${CPACK_GTIRB_VERSION_UPPER_BOUND}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}), libboost-dev (>=1.67) | libboost1.67-dev, libcapstone-dev (=1:4.0.1-gt1)"
+      "libstdc++6, libc6, libgcc1, libgtirb-dev (>=${CPACK_GTIRB_VERSION}), libgtirb-dev (<<${CPACK_GTIRB_VERSION_UPPER_BOUND}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}), libboost-dev (>=1.67) | libboost1.67-dev, libcapstone-dev (=1:4.0.1-gt2)"
   )
 
 elseif("${CPACK_GTIRB_DEBIAN_PACKAGE}" STREQUAL "driver")
@@ -28,7 +28,7 @@ elseif("${CPACK_GTIRB_DEBIAN_PACKAGE}" STREQUAL "driver")
   set(CPACK_PACKAGE_FILE_NAME "gtirb-pprinter")
   set(CPACK_COMPONENTS_ALL pprinter_driver layout_driver)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libstdc++6, libc6, libgcc1, libgtirb (>=${CPACK_GTIRB_VERSION}), libgtirb (<<${CPACK_GTIRB_VERSION_UPPER_BOUND}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt1)"
+      "libstdc++6, libc6, libgcc1, libgtirb (>=${CPACK_GTIRB_VERSION}), libgtirb (<<${CPACK_GTIRB_VERSION_UPPER_BOUND}), libgtirb-pprinter (=${CPACK_GTIRB_PPRINTER_VERSION}), libboost (>=1.67) | libboost1.67, libcapstone-dev (=1:4.0.1-gt2)"
   )
 elseif("${CPACK_GTIRB_DEBIAN_PACKAGE}" STREQUAL "driver-dbg")
   set(CPACK_DEBIAN_PACKAGE_NAME "gtirb-pprinter-dbg")
