9ce03bead31a23f761bb076d93a99853daee07f5
Joshua Robbins <jrobbins@grammatech.com>
2020-11-13 09:34:08 -0500

    Fix formatting

--- src/gtirb_pprinter/Mips32PrettyPrinter.cpp
+++ src/gtirb_pprinter/Mips32PrettyPrinter.cpp
@@ -53,8 +53,8 @@ Mips32PrettyPrinter::Mips32PrettyPrinter(gtirb::Context& context_,
                                          const PrintingPolicy& policy_)
     : ElfPrettyPrinter(context_, module_, syntax_, policy_) {
   // Setup Capstone.
-  [[maybe_unused]] cs_err err =
-      cs_open(CS_ARCH_MIPS, CS_MODE_MIPS32 | CS_MODE_BIG_ENDIAN, &this->csHandle);
+  [[maybe_unused]] cs_err err = cs_open(
+      CS_ARCH_MIPS, CS_MODE_MIPS32 | CS_MODE_BIG_ENDIAN, &this->csHandle);
   assert(err == CS_ERR_OK && "Capstone failure");
 }
 
