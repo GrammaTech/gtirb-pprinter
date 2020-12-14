d8fd180853e19e413810eda7205aef24a1f71d20
Junghee Lim <junghee@grammatech.com>
2020-12-14 13:10:24 -0500

    Fix format
    -- ok self @ip:debloat

--- include/gtirb_pprinter/AuxDataSchema.hpp
+++ include/gtirb_pprinter/AuxDataSchema.hpp
@@ -89,9 +89,8 @@ struct PeExportedSymbols {
 /// \brief Auxiliary data for extra symbol info.
 struct ElfSymbolInfo {
   static constexpr const char* Name = "elfSymbolInfo";
-  typedef std::map<
-      gtirb::UUID,
-      std::tuple<uint64_t, std::string, std::string, std::string, uint64_t>>
+  typedef std::map<gtirb::UUID, std::tuple<uint64_t, std::string, std::string,
+                                           std::string, uint64_t>>
       Type;
 };
 
