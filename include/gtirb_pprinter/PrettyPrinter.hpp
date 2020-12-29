6a8d83dad4ba87082d35487e904d4e2a21899aeb
Kevin Warrick <kwarrick@grammatech.com>
2020-12-29 10:17:01 -0500

    Revert "Revert "Make CFI directives stateful""
    This reverts commit 20d55440a44b4570f523cd42ff68d43e7cb74647.

--- include/gtirb_pprinter/PrettyPrinter.hpp
+++ include/gtirb_pprinter/PrettyPrinter.hpp
@@ -399,6 +399,8 @@ private:
   std::set<gtirb::Addr> functionLastBlock;
   gtirb::Addr programCounter;
 
+  std::optional<gtirb::Addr> CFIStartProc;
+
   template <typename BlockType>
   void printBlockImpl(std::ostream& os, BlockType& block);
 };
