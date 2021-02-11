f6f4b3f694c98f3359ee348b8d3db3629f79153e
Kevin Warrick <kwarrick@grammatech.com>
2021-02-11 10:56:55 -0500

    Remove implicit underscore

--- src/gtirb_pprinter/PeBinaryPrinter.cpp
+++ src/gtirb_pprinter/PeBinaryPrinter.cpp
@@ -274,7 +274,11 @@ void PeBinaryPrinter::prepareLinkerArguments(
     if (gtirb::CodeBlock* Block = Iter->getEntryPoint();
         Block && Block->getAddress()) {
       auto entry_syms = Iter->findSymbols(*Block->getAddress());
-      args.push_back("/entry:" + (&*entry_syms.begin())->getName());
+      std::string Name = (&*entry_syms.begin())->getName();
+      if (Iter->getISA() == gtirb::ISA::IA32 && Name.size() && Name[0] == '_') {
+        Name = Name.substr(1);
+      }
+      args.push_back("/entry:" + Name);
     } else {
       args.push_back("/NOENTRY");
     }
