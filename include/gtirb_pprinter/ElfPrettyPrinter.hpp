cd11ef7db591e83258c432d09718f1106dc82c5b
Joshua Robbins <jrobbins@grammatech.com>
2021-04-14 10:45:40 -0400

    Align exported data blocks

--- include/gtirb_pprinter/ElfPrettyPrinter.hpp
+++ include/gtirb_pprinter/ElfPrettyPrinter.hpp
@@ -128,6 +128,11 @@ protected:
   void printSymbolHeader(std::ostream& os, const gtirb::Symbol& symbol);
 
   std::optional<uint64_t> getAlignment(const gtirb::CodeBlock& Block) override;
+  std::optional<uint64_t> getAlignment(const gtirb::DataBlock& Block) override;
+
+private:
+  template <typename BlockType>
+  std::optional<uint64_t> getAlignmentImpl(const BlockType& Block);
 };
 
 class DEBLOAT_PRETTYPRINTER_EXPORT_API ElfPrettyPrinterFactory
