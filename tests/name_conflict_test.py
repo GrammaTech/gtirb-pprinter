c909f21e80b2f585816e1a926e7c4d485475cd37
Ariel Davis <adavis@grammatech.com>
2022-01-26 17:03:32 -0600

    added tests for changes to symbol renaming

---
+++ tests/name_conflict_test.py
@@ -0,0 +1,54 @@
+import unittest
+from gtirb_helpers import (
+    add_text_section,
+    create_test_module,
+    add_function,
+    add_code_block,
+)
+import pprinter_helpers
+import gtirb
+
+###
+
+
+def create_ir_with_name(name: str) -> gtirb.IR:
+    (ir, m) = create_test_module(
+        gtirb.Module.FileFormat.ELF, gtirb.Module.ISA.X64
+    )
+    _, bi = add_text_section(m)
+    cb = add_code_block(bi, b"\xC3")
+    add_function(m, name, cb)
+    return ir
+
+
+class NameConflictTest(unittest.TestCase):
+    def test_di_att(self):
+        ir = create_ir_with_name("di")
+        asm = pprinter_helpers.run_asm_pprinter(ir, ["--syntax", "att"])
+        self.assertIn("di:", asm)
+
+    def test_di_intel(self):
+        ir = create_ir_with_name("di")
+        asm = pprinter_helpers.run_asm_pprinter(
+            ir, ["--syntax", "intel", "--format", "raw"]
+        )
+        self.assertIn("di_renamed:", asm)
+
+    def test_di_masm(self):
+        ir = create_ir_with_name("di")
+        asm = pprinter_helpers.run_asm_pprinter(
+            ir, ["--syntax", "masm", "--format", "raw"]
+        )
+        self.assertIn("di_renamed:", asm)
+
+    def test_div_intel(self):
+        ir = create_ir_with_name("div")
+        asm = pprinter_helpers.run_asm_pprinter(ir, ["--syntax", "intel"])
+        self.assertIn("div:", asm)
+
+    def test_div_masm(self):
+        ir = create_ir_with_name("div")
+        asm = pprinter_helpers.run_asm_pprinter(
+            ir, ["--syntax", "masm", "--format", "raw"]
+        )
+        self.assertIn("div_renamed:", asm)
