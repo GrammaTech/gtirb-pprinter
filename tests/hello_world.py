5c2dde11b9f24a4cb66a8ae1ee7b305d56da93a9
Kevin Warrick <kwarrick@grammatech.com>
2022-02-02 12:37:53 -0500

    Replace test-install two_module.gtirb

---
+++ tests/hello_world.py
@@ -0,0 +1,49 @@
+"""
+Build a minimal X86-64 hello world GTIRB file.
+"""
+
+import gtirb
+
+from gtirb_helpers import (
+    create_test_module,
+    add_elf_symbol_info,
+    add_text_section,
+    add_data_section,
+    add_symbol,
+    add_code_block,
+    add_data_block,
+)
+
+ir, m = create_test_module(gtirb.Module.FileFormat.ELF, gtirb.Module.ISA.X64)
+
+# Add .data section.
+s, bi = add_data_section(m, 0x4000A8)
+block = add_data_block(bi, b"hello world\n")
+hello = add_symbol(m, "hello", block)
+
+# Add .text section.
+s, bi = add_text_section(m, 0x400080)
+
+# mov eax, 1
+block = add_code_block(bi, b"\xB8\x01\x00\x00\x00")
+start = add_symbol(m, "_start", block)
+add_elf_symbol_info(m, start, block.size, "FUNC")
+
+# mov ebx, 1
+add_code_block(bi, b"\xBB\x01\x00\x00\x00")
+# mov rsi, hello
+operand = gtirb.SymAddrConst(0, hello)
+add_code_block(bi, b"\x48\xBE\xA8\x00\x40\x00\x00\x00\x00\x00", {2: operand})
+# mov rsi, 13
+add_code_block(bi, b"\xBA\x0D\x00\x00\x00")
+# syscall
+add_code_block(bi, b"\x0F\x05")
+# mov eax, 60
+add_code_block(bi, b"\xB8\x3C\x00\x00\x00")
+# mov edi, 0
+add_code_block(bi, b"\xBF\x00\x00\x00\x00")
+# syscall
+add_code_block(bi, b"\x0F\x05")
+
+# Save GTIRB file.
+ir.save_protobuf("hello.gtirb")
