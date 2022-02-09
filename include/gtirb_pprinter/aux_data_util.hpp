1ec3bc5cd1d0ae680c6f1856882e30acef696cef
Ariel Davis <adavis@grammatech.com>
2022-02-09 18:56:36 +0000

    Apply 29 suggestion(s) to 1 file(s)

--- include/gtirb_pprinter/aux_data_util.hpp
+++ include/gtirb_pprinter/aux_data_util.hpp
@@ -49,6 +49,8 @@
 namespace aux_data {
 
 namespace util {
+
+// Dereference an AuxData table or initialize a default value.
 template <typename Schema>
 typename Schema::Type getOrDefault(const typename Schema::Type* SchemaPtr) {
   if (SchemaPtr) {
@@ -57,11 +59,13 @@ typename Schema::Type getOrDefault(const typename Schema::Type* SchemaPtr) {
   return {};
 }
 
+// Load an AuxData table from a Module or a default value.
 template <typename Schema>
 typename Schema::Type getOrDefault(const gtirb::Module& Module) {
   return getOrDefault<Schema>(Module.getAuxData<Schema>());
 }
 
+// Access a map-typed AuxData schema by key value.
 template <typename Schema, typename KeyType>
 std::optional<typename Schema::Type::mapped_type>
 getByKey(KeyType K, const typename Schema::Type* SchemaPtr) {
@@ -73,12 +77,14 @@ getByKey(KeyType K, const typename Schema::Type* SchemaPtr) {
   return std::nullopt;
 }
 
+// Access a map-typed AuxData schema by key value.
 template <typename Schema>
 std::optional<typename Schema::Type::mapped_type>
 getByOffset(const gtirb::Offset Offset, const gtirb::Module& Mod) {
   return getByKey<Schema, gtirb::Offset>(Offset, Mod.getAuxData<Schema>());
 }
 
+// Access a map-typed AuxData keyed by gtirb::Offset.
 template <typename Schema>
 std::optional<typename Schema::Type::mapped_type>
 getByNode(const gtirb::Node& Node, const gtirb::Module& Mod) {
@@ -89,6 +95,8 @@ getByNode(const gtirb::Node& Node, const gtirb::Module& Mod) {
 }; // namespace util
 
 namespace elf {
+
+// Table maps ELF flag labels to assembly keywords.
 static const std::unordered_map<std::string, std::string> TypeNameConversion = {
     {"FUNC", "function"},  {"OBJECT", "object"},
     {"NOTYPE", "notype"},  {"NONE", "notype"},
@@ -97,6 +105,7 @@ static const std::unordered_map<std::string, std::string> TypeNameConversion = {
 
 }; // namespace elf
 
+// Type wrapper for ELF symbol properties stored in the `elfSymbolInfo' table.
 struct ElfSymbolInfo {
   using AuxDataType =
       std::tuple<uint64_t, std::string, std::string, std::string, uint64_t>;
@@ -125,6 +134,7 @@ struct ElfSymbolInfo {
   }
 };
 
+// Type wrapper for CFI directives stored in the `.cfiDirectives' table.
 struct CFIDirective {
   using AuxDataType =
       std::tuple<std::string, std::vector<int64_t>, gtirb::UUID>;
@@ -142,61 +152,86 @@ struct CFIDirective {
 
 std::optional<std::string> getEncodingType(const gtirb::DataBlock& DataBlock);
 
-// There's an extra vector allocated here
-// that doesn't need to be, but getting c++
-// to structure the output without allocating a new
-// vector is a lot of work
+// Find CFI directives for some location in a byte interval in the
+// `cfiDirectives' AuxData table.
 std::optional<std::vector<CFIDirective>>
 getCFIDirectives(const gtirb::Offset& Offset, const gtirb::Module& Mod);
 
+// Load all function entry nodes from the `functionEntries' Auxdata table.
 gtirb::schema::FunctionEntries::Type
 getFunctionEntries(const gtirb::Module& Mod);
 
+// Load all function block UUIDs from the `functionBlocks' AuxData table.
 std::map<gtirb::UUID, std::set<gtirb::UUID>>
 getFunctionBlocks(const gtirb::Module& Mod);
 
+// Find the size of a symbolic expression by offset (`symbolicExpressionSizes').
 std::optional<uint64_t> getSymbolicExpressionSize(const gtirb::Offset& Offset,
                                                   const gtirb::Module& Mod);
 
+// Load all alignment entries from the `alignment' AuxData table.
 gtirb::schema::Alignment::Type getAlignments(const gtirb::Module& Mod);
 
 std::optional<uint64_t> getAlignment(const gtirb::UUID& Uuid,
                                      const gtirb::Module& Mod);
 
+// Find a mapping of one symbol to another in the `symbolForwarding' AuxData
+// table.
 std::optional<gtirb::UUID> getForwardedSymbol(const gtirb::Symbol* Symbol);
 
+// Load all library names from the `libraries' AuxData table.
 std::vector<std::string> getLibraries(const gtirb::Module& Module);
 
+// Load all library path names from the `libraryPaths' AuxData table.
 std::vector<std::string> getLibraryPaths(const gtirb::Module& Module);
 
+// Load all binary type specifiers from the `binaryType' AuxData table.
 std::vector<std::string> getBinaryType(const gtirb::Module& Module);
 
+// Load symbol forwarding mapping from the `symbolForwarding' AuxData table.
 std::map<gtirb::UUID, gtirb::UUID>
 getSymbolForwarding(const gtirb::Module& Module);
 
+// Load all comments for instructions from the `comments' AuxData table.
 const gtirb::schema::Comments::Type* getComments(const gtirb::Module& Module);
 
+// Check that a Module's AuxData contains all tables required for printing.
 bool validateAuxData(const gtirb::Module& Mod, std::string TargetFormat);
 
+// Load the properties of a symbol from the `elfSymbolInfo' AuxData table.
 std::optional<aux_data::ElfSymbolInfo>
 getElfSymbolInfo(const gtirb::Symbol& Sym);
 
+// Store the properties of a symbol to the `elfSymbolInfo' AuxData table.
 void setElfSymbolInfo(gtirb::Symbol& Sym, aux_data::ElfSymbolInfo& Info);
 
+// Load the section properties of an ELF binary section from the
+// `elfSectionProperties' AuxData tables.
 std::optional<std::tuple<uint64_t, uint64_t>>
 getElfSectionProperties(const gtirb::Section& Section);
 
+// Load the section properties of a PE binary section from the
+// `elfSectionProperties' AuxData tables.
 std::optional<uint64_t> getPeSectionProperties(const gtirb::Section& Section);
 
+// Load list UUIDs for symbols imported by a PE binary from the
+// `peImportedSymbols' AuxData table.
 gtirb::schema::ImportEntries::Type getImportEntries(const gtirb::Module& M);
 
+// Load list UUIDs for symbols exported by a PE binary from the
+// `peExportedSymbols' AuxData table.
 gtirb::schema::ExportEntries::Type getExportEntries(const gtirb::Module& M);
 
+// Load all PE resources from the `peResources' AuxData table.
 gtirb::schema::PEResources::Type getPEResources(const gtirb::Module& M);
 
+// Load all imported symbol properties for a PE binary from the
+// `peImportEntries' AuxData table.
 gtirb::schema::PeImportedSymbols::Type
 getPeImportedSymbols(const gtirb::Module& M);
 
+// Load all exported symbol properties for a PE binary from the
+// `peExportEntries' AuxData table.
 gtirb::schema::PeExportedSymbols::Type
 getPeExportedSymbols(const gtirb::Module& M);
 
