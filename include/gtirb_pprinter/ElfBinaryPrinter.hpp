c2982e4671e2bf6e239c9fc6e8479bc19136e61e
Adam Seitz <aseitz@grammatech.com>
2023-05-05 11:27:56 -0400

    Add docstrings for dummy SO methods

--- include/gtirb_pprinter/ElfBinaryPrinter.hpp
+++ include/gtirb_pprinter/ElfBinaryPrinter.hpp
@@ -39,9 +39,41 @@ private:
   std::optional<std::string>
   findLibrary(const std::string& library,
               const std::vector<std::string>& paths) const;
+
+  /**
+  Generate a dummy stand-in library defining the symbols specified in syms.
+
+  Symbols in a group together will be generated refer to the same location in
+  the library.
+
+  Creates a library with the filename lib in the directory libDir. Appends
+  compiler arguments to libArgs required for linking with the generated
+  library.
+
+  Returns true on success, or false if:
+  - libDir does not exist
+  - elfSymbolInfo auxdata cannot be found for a symbol in syms
+  - Symbols in the same SymbolGroup have inconsistent sizes
+  - The compiler returned an error when building the dummy .so
+  */
   bool generateDummySO(const gtirb::IR& ir, const std::string& libDir,
                        const std::string& lib,
                        const std::vector<SymbolGroup>& syms) const;
+
+  /**
+  Generate dummy stand-in libraries for .so files, so that original libraries
+  are not needed to re-link the binary.
+
+  Libraries are generated in the libDir directory. Appends compiler arguments
+  to libArgs required for linking with the generated libraries.
+
+  Returns true on success, or false if:
+  - generateDummySO fails (see its docstring for failure reasons)
+  - There are no dynamic libraries needed
+  - Symbols in the same group have conflicting elfSymbolVersionInfo
+  - There are not enough external symbols to generate all of the dynamically
+    linked libraries
+  */
   bool prepareDummySOLibs(const gtirb::Context& Context, const gtirb::IR& ir,
                           const std::string& libDir,
                           std::vector<std::string>& libArgs) const;
